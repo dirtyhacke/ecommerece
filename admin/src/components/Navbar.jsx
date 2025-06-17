e40ecaaa2c1fb27234e5af6a4ebad23a9215b6de
swaraj <swarajcn774@gmail.com>
2025-06-17 09:43:25 +0530

    ui-update

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = ({setToken}) => {
     <div className='flex items-center py-2 px-[4%] justify-between'>
 
         <img className='w-[max(10%,80px)]'  src={assets.logo} alt="" />
-        <button onClick={()=>setToken('')}  className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm hover:bg-gray-700'>Logout</button>
+        <button onClick={()=>setToken('')}  className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm hover:bg-gray-700 transition duration-700 ease-in-out'>Logout</button>
     </div>
   )
 }
