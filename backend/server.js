84c1ccd1557fd54f040c0108e65c9ed3226b02b6
swaraj <swarajcn774@gmail.com>
2025-07-28 04:48:31 +0530

    qwertyui

--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,35 @@
 import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
-import connectDB from './config/mongodb.js';
-import connectCloudinary from './config/cloudinary.js';
-import userRouter from './routes/userRoute.js';
-import productRouter from './routes/productRoute.js';
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+import userRouter from './routes/userRoute.js'
+import productRouter from './routes/productRoute.js'
 import cartRouter from './routes/cartRoute.js'
-import orderRouter from './routes/orderRoute.js';
-//APP CONFIG
+import orderRouter from './routes/orderRoute.js'
 
-const app = express();
+//app config
+
+const app = express()
 const port = process.env.PORT || 4000
-connectDB();
-connectCloudinary();
-
-// MIDDLE WARES 
+connectDB()
+connectCloudinary()
+// middile wares
 
 app.use(express.json())
 app.use(cors())
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
-// API END POINTS
 
+// api end points
 app.use('/api/user',userRouter)
 
 app.use('/api/product',productRouter)
 
+app.use('/api/cart',cartRouter)
+app.use('/api/order',orderRouter)
 
-   
 app.get('/',(req,res)=>{
-  res.send('SERVER WORKING')
+    res.send("API WORKING")
 })
 
-app.listen(port,()=>console.log('SERVER IS RUNNING PORT 4000 AND'));
+app.listen(port,()=> console.log("server started on port :" + port))
+
